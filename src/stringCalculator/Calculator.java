d91ecfcfdd54b1a7f003c8a1d2045162c6092514
Danish Raza <rdanishraza@gmail.com>
2021-01-14 17:18:41 +0530

    added code for first step of calculator add function for which I have commited test part already

--- src/stringCalculator/Calculator.java
+++ src/stringCalculator/Calculator.java
@@ -2,6 +2,14 @@ package stringCalculator;
 
 public class Calculator {
 	public int add(String arg){
-		return 0;
+		if(arg == null || arg.isEmpty()){
+			return 0;
+		}
+		String[] numbers = arg.split(",");
+		int sum = 0;
+		for(String number : numbers){
+			sum += Integer.parseInt(number);
+		}
+		return sum;
 	} 
 }
