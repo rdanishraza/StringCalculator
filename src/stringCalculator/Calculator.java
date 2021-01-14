14b36dd122ccaf0b0264574c16566e7ae26a30fc
Danish Raza <rdanishraza@gmail.com>
2021-01-14 21:37:27 +0530

    complete implementation of step 2

--- src/stringCalculator/Calculator.java
+++ src/stringCalculator/Calculator.java
@@ -8,6 +8,9 @@ public class Calculator {
 		String[] numbers = arg.split(",");
 		int sum = 0;
 		for(String number : numbers){
+			if(Integer.parseInt(number) > Integer.MAX_VALUE){
+				return 0;
+			}
 			sum += Integer.parseInt(number);
 		}
 		return sum;
